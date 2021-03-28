9cdf0049e2d8a21c902ce01e69046233685bf440
RWitak <rafael.witak@gmail.com>
2021-03-28 17:36:25 +0200

    Extract cli logic to cli.py, add copyright

--- src/boost2simple/converter.py
+++ src/boost2simple/converter.py
@@ -14,71 +14,14 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import argparse
-import os
-import pathlib
-
-from notes.boostnote import BoostNote
-from notes.simplenote import SimpleNote
-
-description = f"{os.path.basename(__file__)} takes one or more " \
-              f"BoostNote collection exports (JSON format) \n" \
-              f"and converts them to a single, " \
-              f"SimpleNote-style export zip-file \n" \
-              f"which can then easily be imported with SimpleNote."
-
-epilog = ("'--no-markdown' sets the SimpleNote's markdown flag to false. \n"
-          "Unless '--no-title' is set, it uses BoostNote's title \n"
-          "as first line of the converted SimpleNote, \n"
-          "followed by a single empty line. \n\n"
-          "'--markdown' sets the SimpleNote's markdown flag to true. \n"
-          "If not set to '--no-title', it also uses \n"
-          "BoostNote's title as first line, \n"
-          "but prefixes it with '# ' and follows up with an empty line; \n"
-          "this effectively turns the BoostNote title into an h1 header. \n\n"
-          "If you want your collection(s) to be tagged "
-          "with their respective names, \n"
-          "it is advised to put them in separate folders within ORIGIN_DIR; \n"
-          "their folder name will be turned into a tag in SimpleNote; \n"
-          "this allows you to intuitively navigate & find them later!")
-
-
-def get_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(
-        description=description,
-        epilog=epilog,
-        formatter_class=argparse.RawTextHelpFormatter)
-
-    parser.add_argument('--markdown',
-                        help="activate/deactivate SimpleNote's markdown flag",
-                        default=True,
-                        action=argparse.BooleanOptionalAction)
-    parser.add_argument('--title',
-                        help="add title of original note to first line "
-                             "or omit it",
-                        default=True,
-                        action=argparse.BooleanOptionalAction)
-    parser.add_argument('--from', metavar="ORIGIN_DIR", nargs=1,
-                        type=pathlib.Path, required=False,
-                        dest='boost_note_dir',
-                        default=[pathlib.Path('')],
-                        help='directory containing BoostNote collection(s) '
-                             '(default: current directory)')
-    parser.add_argument('--to', metavar="TARGET_DIR", nargs=1,
-                        type=pathlib.Path, required=False,
-                        dest='simple_note_dir',
-                        default=[pathlib.Path('')],
-                        help='directory to save the zip-file to '
-                             '(default: current directory)')
-    return parser
-
-
-def sanitize_dir_path(path: pathlib.Path) -> pathlib.Path:
-    if os.path.isdir(path):
-        return path
-    else:
-        print(str(path) + " is not a valid directory name.")
-        exit()
+try:
+    from notes.boostnote import BoostNote
+    from notes.simplenote import SimpleNote
+except ImportError:
+    import sys
+    sys.path.append(sys.path[0] + '/..')
+    from notes.boostnote import BoostNote
+    from notes.simplenote import SimpleNote
 
 
 class Converter:
@@ -106,18 +49,3 @@ class Converter:
             return 0
         else:
             return 1
-
-
-def main():
-    args = get_parser().parse_args()
-    simple_dir = sanitize_dir_path(vars(args).get("simple_note_dir")[0])
-    boost_dir = sanitize_dir_path(vars(args).get("boost_note_dir")[0])
-    md = vars(args).get("markdown", True)
-    title = vars(args).get("title", True)
-    converter = Converter(boost_dir, simple_dir, md, title)
-    converter.convert()
-    print(converter.status)
-
-
-if __name__ == "__main__":
-    main()
